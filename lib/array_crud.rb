7f82b85c195b7b11d9746fa8153cea1e4aad034c
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-10-03 16:40:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array=["pop","goes","weasel"]
+  array=["pop","goes","arrays!"]
   array.pop
 end
 
